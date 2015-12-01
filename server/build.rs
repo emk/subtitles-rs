6a799bd319b739bb49299cf7d33da14756a27437
Eric Kidd <git@randomhacks.net>
2015-11-30 20:36:12 -0500

    ci: Retry `elm make` several times in case of network failure
    This is annoying, and causes a lot of bogus failed builds.
    
    https://github.com/elm-lang/elm-package/issues/163

--- server/build.rs
+++ server/build.rs
@@ -1,6 +1,53 @@
+use std::convert::From;
+use std::error;
+use std::error::Error as StdError;
+use std::fmt;
+use std::fs;
+use std::io;
 use std::process::{Command, exit};
 
-fn main() {
+use ElmMakeError::{CouldNotRun, Failed};
+
+#[derive(Debug)]
+enum ElmMakeError {
+    CouldNotRun(io::Error),
+    Failed,
+}
+
+impl fmt::Display for ElmMakeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let desc = self.description();
+        match self {
+            &CouldNotRun(ref e) =>
+                write!(f, "{}: {}\nAre the elm tools installed?", desc, e),
+            &Failed => write!(f, "{}", desc),
+        }
+    }
+}
+
+impl error::Error for ElmMakeError {
+    fn description(&self) -> &str {
+        match self {
+            &CouldNotRun(_) => "Could not run `elm make`",
+            &Failed => "elm-make build failed",
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match self {
+            &CouldNotRun(ref e) => Some(e),
+            &Failed => None,
+        }
+    }
+}
+
+impl From<io::Error> for ElmMakeError {
+    fn from(err: io::Error) -> ElmMakeError {
+        CouldNotRun(err)
+    }
+}
+
+fn elm_make_once() -> Result<(), ElmMakeError> {
     // Build the command to run.
     let mut process = Command::new("elm");
     process
@@ -11,16 +58,37 @@ fn main() {
         .current_dir("site");
 
     // Try to run the command.
-    let status = process.status().unwrap_or_else(|e| {
-        println!("Error running process: {}", e);
-        println!("You may need to install the elm tools.");
-        exit(1);
-    });
-
-    // Fail if the build failed.
+    let status = try!(process.status());
     if !status.success() {
-        println!("Build failed");
-        exit(1);
+        return Err(Failed);
+    }
+    Ok(())
+}
+
+fn elm_make() -> Result<(), ElmMakeError> {
+    if fs::metadata("site/elm-stuff").is_err() {
+        println!("Will retry `elm make` if it fails.");
+        for i in 0..2 {
+            match elm_make_once() {
+                Err(Failed) => {
+                    println!("Build {} failed, perhaps because of network.", i);
+                    println!("Retrying...");
+                    continue
+                }
+                result => return result,
+            }
+        }
+    }
+    elm_make_once()
+}
+
+fn main() {
+    match elm_make() {
+        Ok(()) => {}
+        Err(ref err) => {
+            println!("{}", err);
+            exit(1);
+        }
     }
 }
 
