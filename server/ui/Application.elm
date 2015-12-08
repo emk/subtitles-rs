df02ea035456cf0a40303010b4adeb55804245cd
Eric Kidd <git@randomhacks.net>
2015-12-08 07:12:39 -0500

    server/ui: Make VideoPlayer a child of Video
    This isn't actually how I want to design this, but given the general
    difficulties that Elm, the "Elm architecture", and even their
    inspiration React have with components, it's probably the cleanest
    solution.

--- server/ui/Application.elm
+++ server/ui/Application.elm
@@ -4,22 +4,19 @@ import Effects exposing (Never)
 import Html exposing (div, text)
 
 import Video
-import VideoPlayer
 import Util exposing (listFromMaybe)
 
 type alias Model =
   { errorMessage: Maybe String
   , video: Maybe Video.Model
-  , player: Maybe VideoPlayer.Model
   }
 
 type Action
-  = VideoLoaded (Maybe Video.Model)
-  | VideoPlayerAction VideoPlayer.Action
+  = VideoLoaded (Maybe (Video.Model, Effects.Effects Video.Action))
   | VideoAction Video.Action
 
 init : (Model, Effects.Effects Action)
-init = (Model Nothing Nothing Nothing, Video.load VideoLoaded)
+init = (Model Nothing Nothing, Video.load VideoLoaded)
 
 update : Action -> Model -> (Model, Effects.Effects Action)
 update msg model =
@@ -29,29 +26,16 @@ update msg model =
         model' = { model | errorMessage = Just "Could not load video" }
       in (model', Effects.none)
 
-    VideoLoaded (Just video) ->
-      let
-        model' = { model | video = Just video, player = Just player }
-        (player, fx) = VideoPlayer.init video.url
-      in (model', Effects.map VideoPlayerAction fx)
-
-    VideoPlayerAction act ->
-      case model.player of
-        Just player ->
-          let
-            (player', fx) = VideoPlayer.update act player
-            model' = { model | player = Just player' }
-          in (model', Effects.map VideoPlayerAction fx)
-        Nothing -> -- Wait what?
-          (model, Effects.none)
+    VideoLoaded (Just (video, fx)) ->
+      ({ model | video = Just video }, Effects.map VideoAction fx)
 
     VideoAction act ->
       case model.video of
         Just video ->
           let
-            video' = Video.update act video
+            (video', fx) = Video.update act video
             model' = { model | video = Just video' }
-          in (model', Effects.none)
+          in (model', Effects.map VideoAction fx)
         Nothing ->
           (model, Effects.none)
 
@@ -60,15 +44,11 @@ view address model =
   let
     flash =
       Maybe.map (\err -> text err) model.errorMessage
-    playerAddr = Signal.forwardTo address VideoPlayerAction
-    player =
-      Maybe.map (\player -> VideoPlayer.view playerAddr player) model.player
-    currentTime = Maybe.withDefault 0 (Maybe.map .currentTime model.player)
     videoAddr = Signal.forwardTo address VideoAction
+    player =
+      Maybe.map (\video -> Video.playerView videoAddr video) model.video
     subtitles =
-      Maybe.map
-        (\video -> Video.subtitlesView videoAddr currentTime video)
-        model.video
+      Maybe.map (\video -> Video.subtitlesView videoAddr video) model.video
     children =
       listFromMaybe flash ++ listFromMaybe player ++ listFromMaybe subtitles
   in div [] children
--- server/ui/Video.elm
+++ server/ui/Video.elm
@@ -1,6 +1,6 @@
-module Video (Model, init, Action, update, subtitlesView, decode, load) where
+module Video
+  (Model, init, Action, update, playerView, subtitlesView, decode, load) where
 
-import Array
 import Effects
 import Html exposing (div)
 import Html.Attributes exposing (class)
@@ -11,23 +11,43 @@ import Task
 import Subtitle
 import Subtitle.Array
 import Util exposing (listFromMaybe)
+import VideoPlayer
 
-type alias Model = { url: String, subtitles: Array.Array Subtitle.Model }
+type alias Model =
+  { url: String
+  , player: VideoPlayer.Model
+  , subtitles: Subtitle.Array.Model
+  }
 
-init : String -> Array.Array Subtitle.Model -> Model
-init url subtitles = Model url subtitles
+init : String -> Subtitle.Array.Model -> (Model, Effects.Effects Action)
+init url subtitles =
+  let
+    (player, fx) = VideoPlayer.init url
+  in (Model url player subtitles, Effects.map Player fx)
 
-type Action = Subtitles Subtitle.Array.Action
+type Action
+  = Subtitles Subtitle.Array.Action
+  | Player VideoPlayer.Action
 
-update : Action -> Model -> Model
+update : Action -> Model -> (Model, Effects.Effects Action)
 update action model =
   case action of
     Subtitles act ->
-      { model | subtitles = Subtitle.Array.update act model.subtitles }
+      ({ model | subtitles = Subtitle.Array.update act model.subtitles },
+       Effects.none)
+    Player act ->
+      let
+        (player', fx) = VideoPlayer.update act model.player
+      in ({ model | player = player' }, Effects.map Player fx)
 
-subtitlesView : Signal.Address Action -> Float -> Model -> Html.Html
-subtitlesView address currentTime model =
+playerView : Signal.Address Action -> Model -> Html.Html
+playerView address model =
+  VideoPlayer.view (Signal.forwardTo address Player) model.player
+
+subtitlesView : Signal.Address Action -> Model -> Html.Html
+subtitlesView address model =
   let
+    currentTime = model.player.currentTime
     idx = Subtitle.Array.timeToIndex currentTime model.subtitles
     addr = Signal.forwardTo address Subtitles
     prev = Subtitle.Array.viewAt (idx - 1) addr model.subtitles
@@ -36,16 +56,16 @@ subtitlesView address currentTime model =
     subs = listFromMaybe prev ++ listFromMaybe curr ++ listFromMaybe next
   in div [class "subtitles"] subs
 
-decode : Json.Decoder Model
+decode : Json.Decoder (Model, Effects.Effects Action)
 decode =
-  Json.object2 Model
+  Json.object2 init
     ("url" := Json.string)
     ("subtitles" := Json.array Subtitle.decode)
 
 videoUrl : String
 videoUrl = "/api/v1/video.json"
 
-load : (Maybe Model -> a) -> Effects.Effects a
+load : (Maybe (Model, Effects.Effects Action) -> a) -> Effects.Effects a
 load toAction =
   Http.get decode videoUrl
     |> Task.toMaybe
