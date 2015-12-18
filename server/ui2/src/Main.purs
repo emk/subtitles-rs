9e9176696c08ae594fbe8b29282cad49238ab066
Eric Kidd <git@randomhacks.net>
2015-12-18 11:05:25 -0500

    ui2: Generate a new skeleton project with 'pulp init'

---
+++ server/ui2/src/Main.purs
@@ -0,0 +1,9 @@
+module Main where
+
+import Prelude
+import Control.Monad.Eff
+import Control.Monad.Eff.Console
+
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  log "Hello sailor!"
