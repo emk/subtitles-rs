4ffe8c0f7544253218b0e3a3cbf2353e90d64a65
Eric Kidd <git@randomhacks.net>
2016-11-06 10:58:48 -0500

    Update to Rust uchardet pre-release
    This may slightly degrade the quality of character set detection, but
    it might get things working on Windows, with any luck.

--- src/decode.rs
+++ src/decode.rs
@@ -4,17 +4,19 @@ use encoding::label::encoding_from_whatwg_label;
 use encoding::types::DecoderTrap;
 use uchardet::detect_encoding_name;
 
-use err::{err_str, Result};
+use err::{err_str, Error, Result};
 
 /// Guess the encoding of a byte buffer and decode it to a string.
 pub fn smart_decode(bytes: &[u8]) -> Result<String> {
-    // If no matching encoding is found, that means we either have
-    // valid ASCII data, or something hopelessly unsalvageable.
-    let name = try!(detect_encoding_name(bytes))
-        .unwrap_or("ascii".to_string());
-    let encoding = try!(encoding_from_whatwg_label(&name).ok_or_else(|| {
-        err_str(format!("Unknown encoding: {}", name))
-    }));
+    let name = try!(detect_encoding_name(bytes)
+        .map_err(|err| {
+            let err: Error = format!("{}", err).into();
+            err
+        }));
+    let encoding = try!(encoding_from_whatwg_label(&name)
+        .ok_or_else(|| {
+            err_str(format!("Unknown encoding: {}", name))
+        }));
     match encoding.decode(bytes, DecoderTrap::Strict) {
         Ok(result) => Ok(result),
         Err(msg) => Err(err_str(msg.into_owned()))
@@ -24,9 +26,9 @@ pub fn smart_decode(bytes: &[u8]) -> Result<String> {
 #[test]
 fn test_smart_decode() {
     assert_eq!("ascii", &smart_decode("ascii".as_bytes()).unwrap());
-    assert_eq!("français",
-               &smart_decode("français".as_bytes()).unwrap());
-    assert_eq!("français",
-               &smart_decode(&[0x66u8, 0x72, 0x61, 0x6e, 0xe7,
-                               0x61, 0x69, 0x73]).unwrap());
+    assert_eq!("élèves français",
+               &smart_decode("élèves français".as_bytes()).unwrap());
+    assert_eq!("“Françoisé”",
+               &smart_decode(&[0x93, 0x46, 0x72, 0x61, 0x6e, 0xe7, 0x6f,
+                               0x69, 0x73, 0xe9, 0x94]).unwrap());
 }
