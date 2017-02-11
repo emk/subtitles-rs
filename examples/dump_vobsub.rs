e85fee6219483f205f2968778e363b8eaf8b6b22
Eric Kidd <git@randomhacks.net>
2017-02-11 18:02:12 -0500

    Provide example which dumps subtitles as images

---
+++ examples/dump_vobsub.rs
@@ -0,0 +1,57 @@
+extern crate docopt;
+#[macro_use]
+extern crate error_chain;
+extern crate image;
+extern crate rustc_serialize;
+extern crate vobsub;
+
+use docopt::Docopt;
+use std::env;
+use std::path::Path;
+use vobsub::{Index, Result};
+
+const USAGE: &'static str = "
+Usage: dump_vobsub [-i] <idx-file>
+
+Options:
+    -i, --images  Dump subtitle image data as PNGs (requires --feature=png)
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    arg_idx_file: String,
+    flag_images: bool,
+}
+
+quick_main!(run);
+
+fn run() -> Result<()> {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.argv(env::args().into_iter()).decode())
+        .unwrap_or_else(|e| e.exit());
+
+    if args.flag_images {
+        check_png_support()?;
+    }
+
+    let path = Path::new(&args.arg_idx_file);
+    let idx = Index::open(path)?;
+    for (i, sub) in idx.subtitles().enumerate() {
+        let sub = sub?;
+        if args.flag_images {
+            let image = sub.to_image(idx.palette());
+            let mut out_path = path.to_owned();
+            out_path.set_extension(&format!("{:04}.png", i));
+            image.save(&out_path)?;
+        }
+    }
+    Ok(())
+}
+
+fn check_png_support() -> Result<()> {
+    if cfg!(feature="png") {
+        Ok(())
+    } else {
+        Err("PNG images disabled, build with `--features png` to enable".into())
+    }
+}
