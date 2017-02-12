b309312dda57b33d779f54c7314d08de351f8d83
Eric Kidd <git@randomhacks.net>
2017-02-12 17:09:03 -0500

    Split vobsub2png binary out a separate crate

--- vobsub2png/src/main.rs
+++ vobsub2png/src/main.rs
@@ -14,16 +14,12 @@ use std::path::{Path, PathBuf};
 use vobsub::{Index, Result, Subtitle};
 
 const USAGE: &'static str = "
-Usage: dump_vobsub [-i] <idx-file>
-
-Options:
-    -i, --images  Dump subtitle image data as PNGs (requires --feature=png)
+Usage: vobsub2png <idx-file>
 ";
 
 #[derive(RustcDecodable)]
 struct Args {
     arg_idx_file: String,
-    flag_images: bool,
 }
 
 quick_main!(run);
@@ -50,30 +46,24 @@ fn run() -> Result<()> {
         .and_then(|d| d.argv(env::args().into_iter()).decode())
         .unwrap_or_else(|e| e.exit());
 
-    if args.flag_images {
-        check_png_support()?;
-    }
-
     let path = Path::new(&args.arg_idx_file);
     let idx = Index::open(path)?;
     let mut info = IndexInfo { subtitles: vec![] };
     for (i, sub) in idx.subtitles().enumerate() {
         let sub: Subtitle = sub?;
-        if args.flag_images {
-            let image = sub.to_image(idx.palette());
-            let mut out_path = path.to_owned();
-            out_path.set_extension(&format!("{:04}.png", i));
-            image.save(&out_path)?;
+        let image = sub.to_image(idx.palette());
+        let mut out_path = path.to_owned();
+        out_path.set_extension(&format!("{:04}.png", i));
+        image.save(&out_path)?;
 
-            info.subtitles.push(SubInfo {
-                start: sub.start_time,
-                end: sub.end_time,
-                force: sub.force,
-                position: (sub.coordinates.left(), sub.coordinates.top()),
-                size: (sub.coordinates.width(), sub.coordinates.height()),
-                path: out_path,
-            });
-        }
+        info.subtitles.push(SubInfo {
+            start: sub.start_time,
+            end: sub.end_time,
+            force: sub.force,
+            position: (sub.coordinates.left(), sub.coordinates.top()),
+            size: (sub.coordinates.width(), sub.coordinates.height()),
+            path: out_path,
+        });
     }
 
     let mut json_path = path.to_owned();
@@ -84,12 +74,4 @@ fn run() -> Result<()> {
     json_file.write_all(json.as_bytes())?;
 
     Ok(())
-}
-
-fn check_png_support() -> Result<()> {
-    if cfg!(feature="png") {
-        Ok(())
-    } else {
-        Err("PNG images disabled, build with `--features png` to enable".into())
-    }
 }
