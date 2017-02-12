8898be0ef0d8c8a32679865ef16cfedfe7220014
Eric Kidd <git@randomhacks.net>
2017-02-12 08:34:02 -0500

    dump_vobsub: Output JSON with subtitle metadata

--- examples/dump_vobsub.rs
+++ examples/dump_vobsub.rs
@@ -8,8 +8,10 @@ extern crate vobsub;
 
 use docopt::Docopt;
 use std::env;
-use std::path::Path;
-use vobsub::{Index, Result};
+use std::fs;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+use vobsub::{Index, Result, Subtitle};
 
 const USAGE: &'static str = "
 Usage: dump_vobsub [-i] <idx-file>
@@ -26,6 +28,21 @@ struct Args {
 
 quick_main!(run);
 
+#[derive(RustcEncodable)]
+struct IndexInfo {
+    subtitles: Vec<SubInfo>,
+}
+
+#[derive(RustcEncodable)]
+struct SubInfo {
+    start: f64,
+    end: f64,
+    force: bool,
+    position: (u16, u16),
+    size: (u16, u16),
+    path: PathBuf,
+}
+
 fn run() -> Result<()> {
     env_logger::init().unwrap();
 
@@ -39,15 +56,33 @@ fn run() -> Result<()> {
 
     let path = Path::new(&args.arg_idx_file);
     let idx = Index::open(path)?;
+    let mut info = IndexInfo { subtitles: vec![] };
     for (i, sub) in idx.subtitles().enumerate() {
-        let sub = sub?;
+        let sub: Subtitle = sub?;
         if args.flag_images {
             let image = sub.to_image(idx.palette());
             let mut out_path = path.to_owned();
             out_path.set_extension(&format!("{:04}.png", i));
             image.save(&out_path)?;
+
+            info.subtitles.push(SubInfo {
+                start: sub.start_time,
+                end: sub.end_time,
+                force: sub.force,
+                position: (sub.coordinates.left(), sub.coordinates.top()),
+                size: (sub.coordinates.width(), sub.coordinates.height()),
+                path: out_path,
+            });
         }
     }
+
+    let mut json_path = path.to_owned();
+    json_path.set_extension("json");
+    let json = rustc_serialize::json::encode(&info)
+        .expect("Cannot encode JSON".into());
+    let mut json_file: fs::File = fs::File::create(&json_path)?;
+    json_file.write_all(json.as_bytes())?;
+
     Ok(())
 }
 
